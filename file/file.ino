6d0032597e408d2312b37819eef0c2fae7adfeb3
Toby J <toby.johnson19@education.nsw.gov.au>
2025-11-17 11:26:47 +1100

    Update file.ino

--- file/file.ino
+++ file/file.ino
@@ -1,1 +1,12 @@
-bitch
+void setup() {
+  lightbar_setup();
+  motion_sensor_setup();
+  ultrasonic_setup();
+}
+
+void loop() {
+  lightbar();
+  motion_sensor();
+  ultrasonic();
+  delay(100);
+}
