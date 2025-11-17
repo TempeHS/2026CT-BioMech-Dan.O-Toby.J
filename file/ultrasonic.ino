cb7faaa241fb6a15f632707de21d17502921640b
Dan O <dan.ozeri@education.nsw.gov.au>
2025-11-17 12:17:49 +1100

    Update ultrasonic.ino

--- file/ultrasonic.ino
+++ file/ultrasonic.ino
@@ -1,19 +1,17 @@
 #include <Ultrasonic.h>
 
 Ultrasonic myUltraSonicSensor(5);
-void setup();
-{
+
+void Ultrasonicsetup() {
   Serial.begin(9600);
 }
 
 
-void loop();
-{
+void UltrasonicFunction() {
   unsigned long RangeInCentimeters;
   RangeInCentimeters = myUltraSonicSensor.distanceRead();
   Serial.print(RangeInCentimeters);
   Serial.println(" cm");
   delay(250);
 
-  
 }
