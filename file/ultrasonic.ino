17ef4861c8bd58313536cfb1a2d547a6e8f9e4f1
Dan O <dan.ozeri@education.nsw.gov.au>
2025-11-17 11:46:05 +1100

    Ultrasonic setup
    ultra sonic sensor setup

---
+++ file/ultrasonic.ino
@@ -0,0 +1,20 @@
+#include "Ultrasonic.h"
+
+
+Ultrasonic myUltraSonicSensor(5);
+void setup()
+{
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  unsigned long RangeInCentimeters;
+  RangeInCentimeters = myUltraSonicSensor.distanceRead();
+  Serial.print(RangeInCentimeters);
+  Serial.println(" cm");
+  delay(250);
+
+  
+}
