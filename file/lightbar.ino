2eaeb57ead704b8c286b0b62ebbf0594298e3066
Toby J <toby.johnson19@education.nsw.gov.au>
2025-11-24 09:35:10 +1100

    Update lightbar.ino

--- file/lightbar.ino
+++ file/lightbar.ino
@@ -17,10 +17,15 @@ void lightbarSetup(){
 void lightbarIncline() {
    while (Incline == true) {
 
+   for (int i = 0; i <= 6; i++) {
     bar.setLed(8, 1);
     bar.setLed(9, 1);
     bar.setLed(10, 1);
+    delay(200); 
    }
+
+   Incline = false;
+   
 }
 
 
