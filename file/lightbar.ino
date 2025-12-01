223a2e78ac585a8ddd7c252340cc75d5d7b6c074
Toby J <toby.johnson19@education.nsw.gov.au>
2025-12-01 11:52:04 +1100

    fixed missing bracket and proper indentation

--- file/lightbar.ino
+++ file/lightbar.ino
@@ -1,34 +1,29 @@
 #include <Grove_LED_Bar.h>
-Grove_LED_Bar bar(9,8, 0);
+Grove_LED_Bar bar(9, 8, 0);
 
 unsigned long previousMillis = 0;
 const long interval = 100;
 int level = 0;
 extern bool Incline;
 
-void lightbarSetup(){
+void lightbarSetup() {
 
-   bar.begin();
-
-
+  bar.begin();
 }
 
 
 void lightbarIncline() {
-   while (Incline == true) {
+  if (Incline == true) {
 
-   for (int i = 0; i <= 6; i++) {
-    bar.setLed(8, 1);
-    bar.setLed(9, 1);
-    bar.setLed(10, 1);
-    delay(300); 
-    bar.setLed(8, 0);
-    bar.setLed(9, 0);
-    bar.setLed(10, 0);
-   }
-
-   Incline = false;
-   
+    for (int i = 0; i <= 6; i++) {
+      bar.setLed(8, 1);
+      bar.setLed(9, 1);
+      bar.setLed(10, 1);
+      delay(300);
+      bar.setLed(8, 0);
+      bar.setLed(9, 0);
+      bar.setLed(10, 0);
+    }
+    Incline = false;
+  }
 }
-
-
