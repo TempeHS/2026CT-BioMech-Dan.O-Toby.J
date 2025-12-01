d7e97f3e8ff8a5c08ffe8da18356f0bd2b37e862
Toby J <toby.johnson19@education.nsw.gov.au>
2025-12-01 12:44:56 +1100

    added documentation and millis() for the lightbar flashing

--- file/lightbar.ino
+++ file/lightbar.ino
@@ -1,9 +1,10 @@
 #include <Grove_LED_Bar.h>
 Grove_LED_Bar bar(9, 8, 0);
-
+unsigned long startMillis;
+unsigned long currentMillis;
 unsigned long previousMillis = 0;
 const long interval = 100;
-int level = 0;
+int period = 300;
 extern bool Incline;
 
 void lightbarSetup() {
@@ -12,19 +13,24 @@ void lightbarSetup() {
   bar.setLevel(0);
 }
 
-
 void lightbarIncline() {
   if (Incline == true) {
 
-    for (int i = 0; i <= 6; i++) {
-      bar.setLed(8, 1);
-      bar.setLed(9, 1);
-      bar.setLed(10, 1);
-      delay(300);
-      bar.setLed(8, 0);
-      bar.setLed(9, 0);
-      bar.setLed(10, 0);
-    }
+    currentMillis = millis();
+
+    if (currentMillis - startMillis >= period)
+
+
+      for (int i = 0; i <= 6; i++) {
+        bar.setLed(8, 1);
+        bar.setLed(9, 1);
+        bar.setLed(10, 1);
+        delay(300);
+        bar.setLed(8, 0);
+        bar.setLed(9, 0);
+        bar.setLed(10, 0);
+      }
     Incline = false;
+    startMillis = currentMillis;
   }
 }
