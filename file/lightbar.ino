d6e37e171ed2a7faabc6eb195c6072daf5161296
Toby J <toby.johnson19@education.nsw.gov.au>
2025-12-08 09:38:05 +1100

    Update lightbar.ino

--- file/lightbar.ino
+++ file/lightbar.ino
@@ -3,9 +3,9 @@ Grove_LED_Bar bar(9, 8, 0);
 unsigned long startMillis;
 unsigned long currentMillis;
 unsigned long previousMillis = 0;
-int barPower = 0;
+bool ledState = false;
 const long interval = 100;
-int period = 500;
+int period = 100;
 extern bool Incline;
 
 void lightbarSetup() {
@@ -15,26 +15,19 @@ void lightbarSetup() {
 }
 
 void lightbarIncline() {
-  if (Incline == true) {
+  if (Incline) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= period) {
+      previousMillis = currentMillis;
+      ledState = !ledState;
 
-    currentMillis = millis();
-
-    if (currentMillis - startMillis >= period)
-
-    previousMillis = currentMillis;
-
-    if (barPower == 0) {
-      barPower = !barPower;
-      bar.setLevel(10, barPower)
-      bar.setLevel(9, barPower)
-      bar.setLevel(8, barPower)
-    } else {
-      bar.setLevel(10, 0)
-      bar.setLevel(9, 0)
-      bar.setLevel(8, 0)
+      bar.setLed(8, ledState);
+      bar.setLed(9, ledState);
+      bar.setLed(10, ledState);
     }
-
-    Incline = false;
-    startMillis = currentMillis;
+  } else {
+    bar.setLed(8, 0);
+    bar.setLed(9, 0);
+    bar.setLed(10, 0);
   }
 }
